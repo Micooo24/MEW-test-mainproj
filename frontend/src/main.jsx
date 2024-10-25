4ae3ad1bb09b3d595fb4b0cee0ec555ee93e79d7
Micooo24 <mico.rabino24@gmail.com>
2024-10-26 00:37:44 +0800

    try lang po

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRouter from './Router';
-import './index.css';
+// src/main.jsx
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <AppRouter />
-    </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Combined App file
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
